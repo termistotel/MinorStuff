62afe71f0fddbf9f9c469da186fe0986c771641f
termistotel <termistotel@gmail.com>
2018-11-07 12:37:41 +0100

    Remove NeuralNet, as it has outgrown it's minor status

--- MachineLearning/NeuralNet/pyLibs/randomInit.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def simpleRandomInit(arh):
-	# Random initialization
-	ws, bs=[],[]
-	for i in range(1,len(arh)):
-		bs.append(np.random.randn(arh[i],1))
-		ws.append(np.random.randn(arh[i-1], arh[i]).T)
-	return ws, bs
